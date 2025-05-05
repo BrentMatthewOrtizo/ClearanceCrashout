8767eab2ecae5366dd2b0faaa3b5c5c1d603446e
mjmpmanny <chapmanny2785@gmail.com>
2025-05-05 15:38:30 -0700

    manny testing push

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -5,7 +5,7 @@ public class Main : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        print("Hello to my world");
     }
 
     // Update is called once per frame
